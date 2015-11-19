b3d7df4463a09c0cc52e058c119b44ac74869c29
Chris Guzman <cguzman@orderup.com>
2015-11-19 16:41:17 -0500

    Initial Commit

---
+++ timbr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  timbr
+//
+//  Created by Chris Guzman on 11/19/15.
+//  Copyright © 2015 Tmber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
