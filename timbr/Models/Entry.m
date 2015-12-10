95a6f806cc9f0d7d0d88172813c219ae02039fb2
Chris Guzman <cguzman@orderup.com>
2015-12-09 21:41:51 -0800

    Clean up!

--- timbr/Models/Entry.m
+++ timbr/Models/Entry.m
@@ -50,4 +50,61 @@
     return entry;
 }
 
++(Entry *)carMockEntry1 {
+    Entry *entry = [[Entry alloc] init];
+    entry.fields = [[NSMutableArray alloc] init];
+    [entry.fields addObjectsFromArray:@[[Field mockField:@80 :@"Mileage" :@"Number"], [Field mockField:@12 :@"Gallons" :@"Decimal"], [Field mockField:@4.25 :@"Gas Price" :@"Currency"]]];
+    return entry;
+}
++(Entry *)carMockEntry2 {
+    Entry *entry = [[Entry alloc] init];
+    entry.fields = [[NSMutableArray alloc] init];
+    [entry.fields addObjectsFromArray:@[[Field mockField:@96 :@"Mileage" :@"Number"], [Field mockField:@8 :@"Gallons" :@"Decimal"], [Field mockField:@2.58 :@"Gas Price" :@"Currency"]]];
+    return entry;
+}
++(Entry *)carMockEntry3 {
+    Entry *entry = [[Entry alloc] init];
+    entry.fields = [[NSMutableArray alloc] init];
+    [entry.fields addObjectsFromArray:@[[Field mockField:@123 :@"Mileage" :@"Number"], [Field mockField:@1 :@"Gallons" :@"Decimal"], [Field mockField:@7.96 :@"Gas Price" :@"Currency"]]];
+    return entry;
+}
+
++(Entry *)fitnessMockEntry1{
+    Entry *entry = [[Entry alloc] init];
+    entry.fields = [[NSMutableArray alloc] init];
+    [entry.fields addObjectsFromArray:@[[Field mockField:@189 :@"Weight" :@"Number"], [Field mockField:@203 :@"Gun size" :@"Number"], [Field mockField:@112 :@"Calories" :@"Number"]]];
+    return entry;
+}
++(Entry *)fitnessMockEntry2 {
+    Entry *entry = [[Entry alloc] init];
+    entry.fields = [[NSMutableArray alloc] init];
+    [entry.fields addObjectsFromArray:@[[Field mockField:@191 :@"Weight" :@"Number"], [Field mockField:@230 :@"Gun size" :@"Number"], [Field mockField:@180 :@"Calories" :@"Number"]]];
+    return entry;
+}
++(Entry *)fitnessMockEntry3 {
+    Entry *entry = [[Entry alloc] init];
+    entry.fields = [[NSMutableArray alloc] init];
+    [entry.fields addObjectsFromArray:@[[Field mockField:@185 :@"Weight" :@"Number"], [Field mockField:@215 :@"Gun size" :@"Number"], [Field mockField:@215 :@"Calories" :@"Number"]]];
+    return entry;
+}
+
++(Entry *)runningMockEntry1 {
+    Entry *entry = [[Entry alloc] init];
+    entry.fields = [[NSMutableArray alloc] init];
+    [entry.fields addObjectsFromArray:@[[Field mockField:@4 :@"Laps" :@"Number"], [Field mockField:@4.5 :@"Lap Time" :@"Decimal"]]];
+    return entry;
+}
++(Entry *)runningMockEntry2 {
+    Entry *entry = [[Entry alloc] init];
+    entry.fields = [[NSMutableArray alloc] init];
+    [entry.fields addObjectsFromArray:@[[Field mockField:@6 :@"Laps" :@"Number"], [Field mockField:@5.25 :@"Lap Time" :@"Decimal"]]];
+    return entry;
+}
++(Entry *)runningMockEntry3 {
+    Entry *entry = [[Entry alloc] init];
+    entry.fields = [[NSMutableArray alloc] init];
+    [entry.fields addObjectsFromArray:@[[Field mockField:@8 :@"Laps" :@"Number"], [Field mockField:@6 :@"Lap Time" :@"Decimal"]]];
+    return entry;
+}
+
 @end
